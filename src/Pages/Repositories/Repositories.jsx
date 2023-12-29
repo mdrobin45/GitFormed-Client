a6961f87f6a2700c947d2cec1d73ccc5fe472e99
Md Robin Rana <developer.smrobin45@gmail.com>
2023-12-29 21:40:25 +0600

    Loader added in table

--- src/Pages/Repositories/Repositories.jsx
+++ src/Pages/Repositories/Repositories.jsx
@@ -7,6 +7,7 @@ import {
    Typography,
 } from "@material-tailwind/react";
 import { useState } from "react";
+import { PulseLoader } from "react-spinners";
 import useRepositories from "../../Hooks/useRepositories";
 import NewRepoModal from "./NewRepoModal/NewRepoModal";
 import ProfileMenu from "./ProfileMenu";
@@ -101,70 +102,76 @@ const Repositories = () => {
             </div>
          </div>
          <Card className={styles.tableWrapper}>
-            <table className={styles.repoTable}>
-               <thead>
-                  <tr className={styles.tableRow}>
-                     {TABLE_HEAD.map((head) => (
-                        <th key={head} className={styles.tableHeader}>
-                           <Typography
-                              variant="small"
-                              color="blue-gray"
-                              className={styles.tableHeadingText}>
-                              {head}
-                           </Typography>
-                        </th>
-                     ))}
-                  </tr>
-               </thead>
-               <tbody>
-                  {repositories.map((repo, index) => {
-                     const isLast = index === repositories.length - 1;
-                     const classes = isLast
-                        ? "p-4"
-                        : "p-4 border-b border-blue-gray-50";
+            {!isPending ? (
+               <table className={styles.repoTable}>
+                  <thead>
+                     <tr className={styles.tableRow}>
+                        {TABLE_HEAD.map((head) => (
+                           <th key={head} className={styles.tableHeader}>
+                              <Typography
+                                 variant="small"
+                                 color="blue-gray"
+                                 className={styles.tableHeadingText}>
+                                 {head}
+                              </Typography>
+                           </th>
+                        ))}
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {repositories.map((repo, index) => {
+                        const isLast = index === repositories.length - 1;
+                        const classes = isLast
+                           ? "p-4"
+                           : "p-4 border-b border-blue-gray-50";
 
-                     return (
-                        <tr key={repo._id}>
-                           <td className={classes}>
-                              <Typography
-                                 variant="small"
-                                 color="blue-gray"
-                                 className="font-normal">
-                                 {repo.repoName}
-                              </Typography>
-                           </td>
-                           <td className={classes}>
-                              <Typography
-                                 variant="small"
-                                 color="blue-gray"
-                                 className="font-normal">
-                                 {repo.repoUsername}
-                              </Typography>
-                           </td>
-                           <td className={classes}>
-                              <Typography
-                                 variant="small"
-                                 color="blue-gray"
-                                 className="font-normal">
-                                 {/* {repo.repoWatchers} */}
-                                 10
-                              </Typography>
-                           </td>
-                           <td className={classes}>
-                              <Typography
-                                 as="a"
-                                 href="#"
-                                 variant="small"
-                                 color="blue-gray"
-                                 className="font-medium">
-                                 Edit
-                              </Typography>
-                           </td>
-                        </tr>
-                     );
-                  })}
-               </tbody>
-            </table>
+                        return (
+                           <tr key={repo._id}>
+                              <td className={classes}>
+                                 <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal">
+                                    {repo.repoName}
+                                 </Typography>
+                              </td>
+                              <td className={classes}>
+                                 <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal">
+                                    {repo.repoUsername}
+                                 </Typography>
+                              </td>
+                              <td className={classes}>
+                                 <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-normal">
+                                    {/* {repo.repoWatchers} */}
+                                    10
+                                 </Typography>
+                              </td>
+                              <td className={classes}>
+                                 <Typography
+                                    as="a"
+                                    href="#"
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="font-medium">
+                                    Edit
+                                 </Typography>
+                              </td>
+                           </tr>
+                        );
+                     })}
+                  </tbody>
+               </table>
+            ) : (
+               <div className="flex py-6 w-full items-start justify-center">
+                  <PulseLoader color="#2563eb" />
+               </div>
+            )}
          </Card>
       </div>
    );
